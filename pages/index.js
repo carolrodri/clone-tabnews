80e503ece147544c7b741e446e67630b950f8895
carolrodri <54365756+carolrodri@users.noreply.github.com>
2025-10-14 14:04:11 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Momozin, eu amo você. Se você me ama, dá uma risadinha! ❤️</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
