e58064beb99e660d7b2d41bca254b95deda8cec9
carolrodri <54365756+carolrodri@users.noreply.github.com>
2025-12-02 21:07:52 +0000

    feat: hide migrator logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrationOptios = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
