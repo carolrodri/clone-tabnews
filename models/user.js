3e4614f5dc706d3456ec06257809eacc9ddf585b
carolrodri <54365756+carolrodri@users.noreply.github.com>
2025-12-05 19:21:51 +0000

    feat: create user.findOneByUsername method and /api/v1/users/[username] endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,36 @@
 import database from "infra/database.js";
-import { ValidationError } from "infra/erros.js";
+import { ValidationError, NotFoundError } from "infra/erros.js";
+
+async function findOneByUserName(username) {
+  const userFound = await runSelectyQuery(username);
+
+  return userFound;
+
+  async function runSelectyQuery(username) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1)
+        LIMIT
+          1
+        ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username está digitado corretamente.",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -69,6 +100,7 @@ async function create(userInputValues) {
 
 const user = {
   create,
+  findOneByUserName,
 };
 
 export default user;
