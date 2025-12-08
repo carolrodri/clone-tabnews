9134c9993c00847be5172af969f5e61a0c80ea71
carolrodri <54365756+carolrodri@users.noreply.github.com>
2025-12-08 17:11:19 +0000

    fix: migrations create user

--- infra/migrations/1764704889363_create-users.js
+++ infra/migrations/1764704889363_create-users.js
@@ -6,11 +6,7 @@ exports.up = (pgm) => {
       default: pgm.func("gen_random_uuid()"),
     },
 
-<<<<<<< HEAD
     // For reference, GitHub liits usernames to 39 characters.
-=======
-    // For reference, GitHub limits usernames to 39 characters.
->>>>>>> 93eb7a1 (feat: hide migrator logs, add runPendingMigrations method to orchestrator and add migration to create users table)
     username: {
       type: "varchar(30)",
       notull: true,
@@ -24,33 +20,18 @@ exports.up = (pgm) => {
     },
 
     password: {
-<<<<<<< HEAD
       type: "varchar(72)",
-=======
-      type: "varchar(60)",
->>>>>>> 93eb7a1 (feat: hide migrator logs, add runPendingMigrations method to orchestrator and add migration to create users table)
       notNull: true,
     },
 
     created_at: {
       type: "timestamptz",
-<<<<<<< HEAD
       default: pgm.func("now()"),
     },
 
     update_at: {
       type: "timestamptz",
       default: pgm.func("now()"),
-=======
-      notNull: true,
-      default: pgm.func("timezone('utc', now())"),
-    },
-
-    updated_at: {
-      type: "timestamptz",
-      notNull: true,
-      default: pgm.func("timezone('utc', now())"),
->>>>>>> 93eb7a1 (feat: hide migrator logs, add runPendingMigrations method to orchestrator and add migration to create users table)
     },
   });
 };
